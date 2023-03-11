0e9474e1b39feec259a29f46ead4a7611a28a0c0
Callum Arnold <24391445+arnold-c@users.noreply.github.com>
2023-03-11 12:56:42 -0500

    Move basic Gillespie and ODE model scripts to src/folder

---
+++ scripts/Gillespie-basic.jl
@@ -0,0 +1,73 @@
+"""
+This is a basic Gillespie simulation of an SIR model.
+It uses the JumpProcesses.jl package and the Direct Gillespie method.
+All jumps are constant rate jumps and are manually defined using JumpProcesses, not using the ModelingToolkit or Catalyst interfaces.
+"""
+
+using DrWatson
+@quickactivate "OutbreakDetection"
+
+using JumpProcesses, Statistics, DataFrames, DataFramesMeta, LinearAlgebra
+using CairoMakie, AlgebraOfGraphics
+
+CairoMakie.activate!()
+set_aog_theme!()
+
+includet(srcdir("Julia/transmission-functions.jl")) # Revise will keep track of file changes and reload functions as necessary
+includet(srcdir("Julia/plotting-functions.jl"))
+includet(srcdir("Julia/cleaning-functions.jl"))
+
+u₀ = [999, 1, 0]
+tspan = (0.0, 250)
+
+R₀ = 2.0
+γ = 1/8
+μ = 1/(62 * 365)
+
+β = calculate_beta(R₀, γ, μ, 1, sum(u₀))
+p = (β, γ, μ)
+
+birth_rate(u, p, t) = p[3] * (u[1] + u[2] + u[3])  # μ*N
+birth_affect!(integrator) = integrator.u[1] += 1  # S -> S + 1
+birth_jump = ConstantRateJump(birth_rate, birth_affect!)
+
+infec_rate(u, p, t) = p[1] * u[1] * u[2]  # β*S*I
+function infec_affect!(integrator)
+    integrator.u[1] -= 1         # S -> S - 1
+    integrator.u[2] += 1         # I -> I + 1
+    nothing
+end
+infec_jump = ConstantRateJump(infec_rate, infec_affect!)
+
+recov_rate(u, p, t) = p[2] * u[2]         # ν*I
+function recov_affect!(integrator)
+    integrator.u[2] -= 1        # I -> I - 1
+    integrator.u[3] += 1        # R -> R + 1
+    nothing
+end
+recov_jump = ConstantRateJump(recov_rate, recov_affect!)
+
+S_death_rate(u, p, t) = p[3] * u[1]  # μ*S
+S_death_affect!(integrator) = integrator.u[1] -= 1  # S -> S + 1
+S_death_jump = ConstantRateJump(S_death_rate, S_death_affect!)
+
+I_death_rate(u, p, t) = p[3] * u[2]  # μ*I
+I_death_affect!(integrator) = integrator.u[2] -= 1  # S -> S + 1
+I_death_jump = ConstantRateJump(I_death_rate, I_death_affect!)
+
+R_death_rate(u, p, t) = p[3] * u[3]  # μ*R
+R_death_affect!(integrator) = integrator.u[3] -= 1  # S -> S + 1
+R_death_jump = ConstantRateJump(R_death_rate, R_death_affect!)
+
+jumps = [birth_jump, infec_jump, recov_jump, S_death_jump, I_death_jump, R_death_jump]
+
+prob = DiscreteProblem(u₀, tspan, p)
+
+jump_prob = JumpProblem(prob, Direct(), jumps...)
+sol = solve(jump_prob, SSAStepper())
+
+sol_df = create_sir_df(sol)
+
+colors = ["dodgerblue4", "firebrick3", "chocolate2", "purple"]
+
+create_sir_plot(sol_df, colors = colors)
---
+++ scripts/ode.jl
@@ -0,0 +1,43 @@
+"""
+This script just performs a simple SIR model using an ODE solver.
+"""
+
+using DrWatson
+@quickactivate "OutbreakDetection"
+
+using DifferentialEquations, Statistics, DataFrames, DataFramesMeta, LinearAlgebra
+using CairoMakie, AlgebraOfGraphics
+
+CairoMakie.activate!()
+set_aog_theme!()
+
+u₀ = [999.0, 1.0, 0.0]
+tspan = (0.0, 250.0)
+
+function sir!(du, u, p, t)
+    (β, γ, μ) = p
+    (S, I, R) = u
+    N = S + I + R
+    du[1] = -β * S * I + μ * (N - S)
+    du[2] = β * S * I - γ * I - μ * I
+    du[3] = γ * I - μ * R
+    nothing
+end
+
+
+R₀ = 2
+γ = 1/8
+μ = 1/(62 * 365)
+β = calculate_beta(R₀, γ, μ, 1, sum(u₀))
+
+p = [β, γ, μ]
+
+ode_prob = ODEProblem(sir!, u₀, tspan, p)
+ode_sol = solve(ode_prob, saveat = 0.1)
+
+
+ode_sol_df = create_sir_df(ode_sol)
+
+colors = ["dodgerblue4", "firebrick3", "chocolate2", "purple"]
+
+create_sir_plot(ode_sol_df; colors = colors)
