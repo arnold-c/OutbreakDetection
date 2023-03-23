ebb9eab8b8d10bdf1144c5a27410824956213a3f
Callum Arnold <24391445+arnold-c@users.noreply.github.com>
2023-03-23 15:38:43 -0400

    Delete old Gillespie code from ODE-MTK script. Need to think about NGM approach Also move state and parameter assignment in dictionaries to after MTK defined the states and parameters to be used in the model - otherwise causes error as Julia tries to find them in the environment

--- scripts/ode-mtk.jl
+++ scripts/ode-mtk.jl
@@ -1,9 +1,7 @@
 """
-This is a Gillespie simulation of an SIR model that has imported infections.
-It uses the JumpProcesses.jl package and the Direct Gillespie method.
-All jumps are constant rate jumps and defined using the ModelingToolkit interface.
+This is a deterministic SIR model using the ModelingToolkit interface.
+The final section of code calculates the R₀ value, as well as the β parameter, for the model, using the Next Generation Matrix method.
 """
-
 #%%
 using DrWatson
 @quickactivate "OutbreakDetection"
@@ -20,12 +18,6 @@ includet(srcdir("Julia/transmission-functions.jl")) # Revise will keep track of 
 includet(srcdir("Julia/plotting-functions.jl"))
 includet(srcdir("Julia/cleaning-functions.jl"))
 
-u₀ = Dict(S => 999, I => 1, R => 0)
-R₀ = 10.0
-
-p = Dict(γ => 1 / 8, μ => 1 / (62 * 365))
-push!(p, β => calculate_beta(R₀, p[μ], p[γ], 1, sum(values(u₀))))
-
 #%%
 δt = 0.005
 tlower = 0.0
@@ -38,9 +30,14 @@ tlength = length(tlower:δt:tmax)
 @variables t S(t) I(t) R(t)
 D = Differential(t)
 
-eqs = [
-    
-D(S) ~ -β * S * I + μ * (I + R), D(I) ~ β * S * I - γ * I - μ * I,
+#%%
+R₀ = 10.0
+p = Dict(γ => 1 / 8, μ => 1 / (62 * 365))
+u₀ = Dict(S => 999, I => 1, R => 0)
+push!(p, β => calculate_beta(R₀, p[μ], p[γ], 1, sum(values(u₀))))
+
+#%%
+eqs = [D(S) ~ -β * S * I + μ * (I + R), D(I) ~ β * S * I - γ * I - μ * I,
     D(R) ~ γ * I - μ * R]
 
 #%%
@@ -50,6 +47,12 @@ de_simple = structural_simplify(de)
 #%%
 ode_prob = ODEProblem(de_simple, u₀, tspan, p)
 ode_sol = solve(ode_prob, Tsit5(); saveat = δt)
+ode_sol_df = create_sir_df(ode_sol)
+
+#%%
+colors = ["dodgerblue4", "firebrick3", "chocolate2", "purple"]
+
+create_sir_plot(ode_sol_df; colors = colors)
 
 #%%
 nic = 1 # number of infective compartments
@@ -73,39 +76,3 @@ eigen(
             (nac + 1):(nac + nic * nac)],
         Dict(S => 0.0, I => 0.0, R => 0.0, p...)),
 )
-
-#%%
-gill_dis_prob = DiscreteProblem(js, u₀, tspan, p)
-gill_jump_prob = JumpProblem(js, gill_dis_prob, Direct(); jac = true)
-gill_sol = solve(gill_jump_prob, SSAStepper())
-
-gill_sol_df = create_sir_df(gill_sol)
-
-#%%
-colors = ["dodgerblue4", "firebrick3", "chocolate2", "purple"]
-
-create_sir_plot(gill_sol_df; colors = colors)
-
-#%%
-nsims = 1000
-
-sir_array = zeros(5, tlength)
-
-all_sims_array = fill(NaN, 5, tlength, nsims)
-
-quantiles = [0.025, 0.05, 0.1, 0.25, 0.5, 0.75, 0.9, 0.95, 0.975]
-
-sim_quantiles = zeros(Float64, length(quantiles), tlength, 4)
-
-gill_jump_prob = JumpProblem(js, gill_dis_prob, Direct(); save_positions = (false, false))
-
-#%%
-create_sir_all_sims_array!(;
-    nsims = nsims, prob = gill_jump_prob, alg = SSAStepper(),
-    δt = δt)
-
-#%%
-create_sir_all_sim_quantiles!(; quantiles = quantiles)
-
-#%%
-create_sir_quantiles_plot!(; lower = 0.1, upper = 0.9, quantiles = quantiles)
