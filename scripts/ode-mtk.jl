9adbc079536ce77177d0dfe1364bb1dd8f04fd33
Callum Arnold <24391445+arnold-c@users.noreply.github.com>
2023-03-24 13:44:33 -0400

    Restructure ODE MTK script to use the NGM calculate beta approach

--- scripts/ode-mtk.jl
+++ scripts/ode-mtk.jl
@@ -31,10 +31,8 @@ tlength = length(tlower:δt:tmax)
 D = Differential(t)
 
 #%%
-R₀ = 10.0
-p = Dict(γ => 1 / 8, μ => 1 / (62 * 365))
-u₀ = Dict(S => 999.0, I => 1.0, R => 0.0)
-push!(p, β => calculate_beta(R₀, p[γ], p[μ], 1, sum(values(u₀))))
+nic = 1 # number of infective compartments
+nac = 1 # number of age classes
 
 #%%
 eqs = [D(S) ~ -β * S * I + μ * (I + R), D(I) ~ β * S * I - γ * I - μ * I,
@@ -43,10 +41,16 @@ eqs = [D(S) ~ -β * S * I + μ * (I + R), D(I) ~ β * S * I - γ * I - μ * I,
 #%%
 @named de = ODESystem(eqs, t, [S, I, R], [β, γ, μ]; tspan = tspan)
 de_simple = structural_simplify(de)
+
+#%%
+R₀ = 10.0
+p = Dict(γ => 1 / 8, μ => 1 / (62 * 365))
+u₀ = Dict(S => 999.0, I => 1.0, R => 0.0)
 push!(
     p,
     β => calculate_beta(;
-        ode = de_simple, R₀ = R₀, param = p, C = [1.0], pop_matrix = [sum(values(u₀))]
+        ode = de_simple, nic = 1, nac = 1, R₀ = R₀, param = p, C = [1.0],
+        pop_matrix = [sum(values(u₀))],
     ),
 )
 
@@ -60,18 +64,5 @@ colors = ["dodgerblue4", "firebrick3", "chocolate2", "purple"]
 
 create_sir_plot(ode_sol_df; colors = colors)
 
-
 #%%
 calculateR0(; ode = de_simple, nic = 1, nac = 1, param = p, S⁺ = 1000.0)
-
-
-
-#%%
-nic = 1 # number of infective compartments
-nac = 1 # number of age classes
-
-#%%
-calculate_beta(;
-    ode = de_simple, nic = 1, nac = 1, R₀ = R₀, param = p, C = [1.0],
-    pop_matrix = [sum(values(u₀))],
-)
