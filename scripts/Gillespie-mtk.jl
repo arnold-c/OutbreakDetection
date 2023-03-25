3b420c27e51160ab273c5cf71dd09e3c584c4a6d
Callum Arnold <24391445+arnold-c@users.noreply.github.com>
2023-03-24 23:00:07 -0400

    Add ensemble to MTK gillespie script

--- scripts/Gillespie-mtk.jl
+++ scripts/Gillespie-mtk.jl
@@ -91,3 +91,23 @@ js_sol_df = create_sir_df(js_sol)
 colors = ["dodgerblue4", "firebrick3", "chocolate2", "purple"]
 
 create_sir_plot(js_sol_df; colors = colors)
+
+#%%
+nsims = 1000
+
+sir_array = zeros(5, tlength)
+all_sims_array = fill(NaN, 5, tlength, nsims)
+
+quantiles = [0.025, 0.05, 0.1, 0.25, 0.5, 0.75, 0.9, 0.95, 0.975]
+sim_quantiles = zeros(Float64, length(quantiles), tlength, 4)
+
+#%%
+gill_jump_prob = JumpProblem(js, de_prob, Direct(); save_positions = (false, false))
+
+create_sir_all_sims_array!(;
+    nsims = nsims, prob = gill_jump_prob, alg = SSAStepper(), δt = δt
+)
+
+create_sir_all_sim_quantiles!(; quantiles = quantiles)
+
+create_sir_quantiles_plot!(; lower = 0.1, upper = 0.9, quantiles = quantiles)
