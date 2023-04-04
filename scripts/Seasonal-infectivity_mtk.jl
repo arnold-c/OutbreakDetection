d892a03e2e7a47534638c94b62015a954565d8b6
Callum Arnold <24391445+arnold-c@users.noreply.github.com>
2023-04-04 14:33:02 -0400

    Update seasonal infectivity with MTK script to use ODEProblem for variable rate jump
    Not 100% sure that the implementation works as expected - solution
    producing additional variable result (Column4) that I'm not sure what it
    is. Also, there seems to be a sudden jump in Recovered individuals that
    doesn't make much sense.
    
    Ensemble problem doesn't work yet.

--- scripts/Seasonal-infectivity_mtk.jl
+++ scripts/Seasonal-infectivity_mtk.jl
@@ -25,46 +25,39 @@ includet(srcdir("Julia/cleaning-functions.jl"))
 tlower = 0.0
 tmax = 250.0
 tspan = (tlower, tmax)
-tlength = length(tlower:δt:tmax)
+tlength = length(tlower:δt:tmax);
 
 #%%
-@parameters β γ μ A_scale
-@variables t S(t) I(t) R(t)
-D = Differential(t)
+@parameters β γ μ A_scale t
+@variables S(t) I(t) R(t)
 
 #%%
-nic = 1 # number of infective compartments
-nac = 1 # number of age classes
-
-#%%
-# Create ODE system for β calculation using NGM
-ode_eqs = [D(S) ~ -β * S * I + μ * (I + R), D(I) ~ β * S * I - γ * I - μ * I,
-    D(R) ~ γ * I - μ * R]
-
-@named de = ODESystem(ode_eqs, t, [S, I, R], [β, γ, μ]; tspan = tspan)
-de_simple = structural_simplify(de)
-
-#%%
+u₀ = [999.0, 1.0, 0.0]
 R₀ = 10.0
-p = Dict(γ => 1 / 8, μ => 1 / (62 * 365), A_scale => 10.0)
-u₀ = Dict(S => 999.0, I => 1.0, R => 0.0)
-push!(p, β => calculate_beta(de_simple, 1, 1, R₀, p, [1.0], [sum(values(u₀))]))
+gamma = 1 / 8
+mu = 1 / (62 * 365)
+amp_scale = 2.0
+beta = calculate_beta(R₀, gamma, mu, ones(1, 1), [sum(u₀)])
+p = (beta, gamma, mu, amp_scale)
 
 #%%
 recov_rate = γ * I
 recov_affect! = [I ~ I - 1, R ~ R + 1]
+
 birth_rate = μ * (S + I + R)
 birth_affect! = [S ~ S + 1]
+
 S_death_rate = μ * S
 S_death_affect! = [S ~ S - 1]
+
 I_death_rate = μ * I
 I_death_affect! = [I ~ I - 1]
+
 R_death_rate = μ * R
 R_death_affect! = [R ~ R - 1]
 
 # Place infection at the end as it is a VariableRateJump, which is ordered after ConstantRateJumps in the dependency graph.
-# A = 0.5 * (cos(2pi * t / 365) + 1)) * 1/scale + (scale - 1)/scale
-A = 0.5 * (cos(2π * t / 365) + 1) * 1 / A_scale + (A_scale - 1) / A_scale
+# A = 0.5 * (cos(2π * t / 365) + 1) * 1 / A_scale + (A_scale - 1) / A_scale
 
 # Lower bound on infection rate
 lrate = 0.0
@@ -72,8 +65,8 @@ lrate = 0.0
 urate = β * S * I
 # Interval that the infection rate must be within the bounds for
 rateinterval = 1 / (2 * urate)
-
-infec_rate = β * S * I * A
+# * (0.5 * (cos(2π * t / 365) + 1) * 1 / A_scale + (A_scale - 1) / A_scale)
+infec_rate = β * S * I * (0.5 * (cos(2π * t / 365) + 1) * 1 / A_scale + (A_scale - 1) / A_scale)
 infec_affect! = [S ~ S - 1, I ~ I + 1]
 
 #%%
@@ -82,38 +75,16 @@ birth_jump = ConstantRateJump(birth_rate, birth_affect!)
 S_death_jump = ConstantRateJump(S_death_rate, S_death_affect!)
 I_death_jump = ConstantRateJump(I_death_rate, I_death_affect!)
 R_death_jump = ConstantRateJump(R_death_rate, R_death_affect!)
-infec_jump = VariableRateJump(infec_rate, infec_affect!; lrate, urate, rateinterval)
+infec_jump = VariableRateJump(infec_rate, infec_affect!; lrate = lrate, urate = urate, rateinterval = rateinterval)
 
 jumps = [recov_jump, birth_jump, S_death_jump, I_death_jump, R_death_jump, infec_jump]
 
 #%%
-# ConstantRateJumps are ordered before VariableRateJumps in the dependency graph, otherwise within the same ordering presented to the JumpProblem, i.e., birth, recovery ...
-# Each row of the dependency graph is a list of rates that must be recalculated when the row's jump is executed, as it affects the underlying states each of the rates depends on.
-dep_graph = [
-    [1, 3, 6],          # Birth, S death, infection
-    [2, 1, 4, 5, 6],    # Recovery, birth, I death, R death, infection
-    [3, 1, 6],          # S death, birth, infection
-    [4, 1, 2, 6],       # I death, birth, recovery, infection
-    [5, 1, 2, 6],       # R death, birth, recovery, infection
-    [6, 1, 2, 3, 4],     # Infection, birth, recovery, S death, I death
-]
-
-#%%
-@named js = JumpSystem(jumps, t, [S, I, R], [β, γ, μ, A_scale]; dep_graph)
-
-#%%
-de_prob = DiscreteProblem(js, u₀, tspan, p)
-js_prob = JumpProblem(de_prob, Coevolve())
-js_sol = solve(js_prob, SSAStepper())
+@named js = JumpSystem(jumps, t, [S, I, R], [β, γ, μ, A_scale])
+ode_prob = ODEProblem((du, u, p, t) -> du .= 0, u₀, tspan, p)
+js_prob = JumpProblem(js, ode_prob, Direct())
+js_sol = solve(js_prob, Tsit5())
 js_sol_df = create_sir_df(js_sol)
-
-#%%
-season_infec_prob = DiscreteProblem([values(u₀)...], tspan, [values(p)...])
-# Bounded VariableJumpRate problems require the Coevolve() algorithm
-season_infec_jump_prob = JumpProblem(season_infec_prob, Coevolve(), jumps...; dep_graph)
-season_infec_sol = solve(season_infec_jump_prob, SSAStepper())
-
-season_infec_sol_df = create_sir_df(season_infec_sol)
 
 #%%
 colors = ["dodgerblue4", "firebrick3", "chocolate2", "purple"]
@@ -126,7 +97,7 @@ t = 0:0.01:720
 y = Vector{Float64}(undef, length(t))
 y_scale = similar(y)
 @. y = 0.5 * (cos(2pi * t / 365) + 1)
-@. y_scale = (0.5 * (cos(2pi * t / 365) + 1)) * 1 / A_scale + (A_scale - 1) / A_scale
+@. y_scale = (0.5 * (cos(2pi * t / 365) + 1)) * 1 / amp_scale + (amp_scale - 1) / amp_scale
 fig = lines(t, y; color = "black", linewidth = 2)
 lines!(t, y_scale; color = "green", linewidth = 2)
 vlines!(0:365:720; color = (:red, 0.5), linestyle = :dash, linewidth = 2)
@@ -139,13 +110,13 @@ nsims = 1000
 sir_array = zeros(4, tlength)
 all_sims_array = fill(NaN, 4, tlength, nsims)
 
-season_infec_jump_prob = JumpProblem(
-    season_infec_prob, Coevolve(), jumps...; dep_graph, save_positions = (false, false)
-)
+season_infec_jump_prob =  JumpProblem(js, ode_prob, Direct(); save_positions = (false, false))
 
 create_sir_all_sims_array!(;
-    nsims = nsims, prob = season_infec_jump_prob, alg = SSAStepper(), δt = δt
+    nsims = nsims, prob = season_infec_jump_prob, alg = Tsit5(), δt = δt
 )
+
+solve(season_infec_jump_prob, Tsit5(); saveat = δt)
 
 #%%
 quantiles = [0.025, 0.05, 0.1, 0.2, 0.25, 0.5, 0.75, 0.8, 0.9, 0.95, 0.975]
@@ -156,3 +127,25 @@ create_sir_all_sim_quantiles!(; quantiles = quantiles)
 
 #%%
 create_sir_quantiles_plot(; lower = 0.1, upper = 0.9, quantiles = quantiles)
+
+# #%%
+# @parameters β γ t
+# @variables S(t) I(t) R(t)
+
+# rate₁ = β * S * I
+# affect₁ = [S ~ S - 1, I ~ I + 1]
+# rate₂ = γ * I * exp(-t / 20)
+# affect₂ = [I ~ I - 1, R ~ R + 1]
+
+# j₁ = ConstantRateJump(rate₁, affect₁)
+# j₂ = VariableRateJump(rate₂, affect₂)
+
+# @named js = JumpSystem([j₁, j₂], t, [S, I, R], [β, γ])
+
+# u₀ = [999.0, 1.0, 0.0];
+# p = (0.1 / 1000, 0.01);
+# tspan = (0.0, 250.0);
+
+# oprob = ODEProblem((du, u, p, t) -> du .= 0, u₀, tspan, p)
+# jprob = JumpProblem(js, oprob, Direct())
+# sol = solve(jprob, Tsit5())
