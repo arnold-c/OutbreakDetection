11ff66ca121e57c558b0e0744519347a312657e1
Callum Arnold <24391445+arnold-c@users.noreply.github.com>
2023-09-06 17:46:15 -0400

    Move Makie setup out of file that is called in module and into own script that is loaded
    The global space isn't altered by the code within the module

--- src/plotting-functions.jl
+++ src/plotting-functions.jl
@@ -12,12 +12,6 @@ using UnPack
 using DataFrames
 using FLoops
 
-set_aog_theme!()
-# Set depending on size of screen
-update_theme!(; resolution = (2200, 1300))
-#= update_theme!(; resolution = (850, 600)) =#
-GLMakie.activate!(; float = true)
-
 seircolors = ["dodgerblue4", "green", "firebrick3", "chocolate2", "purple"]
 seir_state_labels = ["S", "E", "I", "R", "N"]
 
