af6d90b5d45c28ae12fd2b11121768e9386ea2ba
Callum Arnold <24391445+arnold-c@users.noreply.github.com>
2023-09-07 18:56:42 -0400

    Update creation of ensemble parameter combinations to use the updated struct definitions
    Need to create the state and dynamics structs at the same time as the ensemble structs as they all rely on the same N value.
    If created separetly, combinations would be created where the dynamics parameters do not match the state parameters they are linked with

--- scripts/ensemble-sim.jl
+++ scripts/ensemble-sim.jl
@@ -10,22 +10,39 @@ using .OutbreakDetection
 
 #%%
 model_types_vec = [("seasonal-infectivity-import", "tau-leaping")]
+
+#%%
 N_vec = [500_000]
 nsims_vec = [1_000]
-init_states_prop_dict = [Dict(:s_prop => 0.1, :e_prop => 0.01, :i_prop => 0.01, :r_prop => 0.88)]
+init_states_prop_dict = [
+    Dict(:s_prop => 0.1, :e_prop => 0.01, :i_prop => 0.01, :r_prop => 0.88)
+]
+
+ensemble_state_p_vec = create_combinations_vec(
+    StateParameters, (N_vec, init_states_prop_dict)
+)
+
+#%%
 tmin_vec = [0.0]
 tstep_vec = [1.0]
 tmax_vec = [365.0 * 100]
 
-time_p_vec = vec(map(Iterators.product(tmin_vec, tstep_vec, tmax_vec)) do (tmin, tstep, tmax)
-    SimTimeParameters(; tmin = tmin, tmax = tmax, tstep = tstep)
-end)
+time_p_vec = vec(
+    map(
+        Iterators.product(tmin_vec, tstep_vec, tmax_vec)
+    ) do (tmin, tstep, tmax)
+        SimTimeParameters(; tmin = tmin, tmax = tmax, tstep = tstep)
+    end,
+)
 
+#%%
 beta_force_vec = collect(0.0:0.1:0.4)
-births_per_k_min = 5
-births_per_k_max = 20
-births_per_k_step = 5
-births_per_k_vec = collect(births_per_k_min:births_per_k_step:births_per_k_max)
+annual_births_per_k_min = 5
+annual_births_per_k_max = 20
+annual_births_per_k_step = 5
+annual_births_per_k_vec = collect(
+    annual_births_per_k_min:annual_births_per_k_step:annual_births_per_k_max
+)
 seed = 1234
 
 #%%
@@ -35,16 +52,46 @@ R_0_vec = [10.0]
 sigma_vec = 1 ./ latent_per_days_vec
 gamma_vec = 1 ./ dur_inf_days_vec
 
-ensemble_base_dynamics_p_vec = create_combinations_vec(
-    DynamicsParameters,
-    (sigma_vec, gamma_vec, R_0_vec),
-)
-
 #%%
-ensemble_spec_vec = create_combinations_vec(
-    EnsembleSpecification,
-    (model_types_vec, N_vec, init_states_prop_dict, ensemble_base_dynamics_p_vec, births_per_k_vec, beta_force_vec, time_p_vec, nsims_vec),
+ensemble_spec_combinations = Iterators.product(beta_force_vec,
+    sigma_vec,
+    gamma_vec,
+    annual_births_per_k_vec,
+    R_0_vec,
+    N_vec,
+    init_states_prop_dict,
+    model_types_vec,
+    time_p_vec,
+    nsims_vec
 )
+ensemble_spec_vec = Vector(undef, length(ensemble_spec_combinations))
+
+for (
+    i, (beta_force, sigma, gamma, annual_births_per_k, R_0, N, init_states_prop, model_type, time_p, nsims)
+) in enumerate(ensemble_spec_combinations)
+    mu = calculate_mu(annual_births_per_k)
+    beta_mean = calculate_beta(R_0, gamma, mu, 1, N)
+    epsilon = calculate_import_rate(mu, R_0, N)
+
+    ensemble_spec_vec[i] = EnsembleSpecification(
+        model_type,
+        StateParameters(
+            N, init_states_prop
+        ),
+        DynamicsParameters(
+            beta_mean,
+            beta_force,
+            sigma,
+            gamma,
+            mu,
+            annual_births_per_k,
+            epsilon,
+            R_0,
+        ),
+        time_p,
+        nsims,
+    )
+end
 
 #%%
 base_param_dict = @dict(
