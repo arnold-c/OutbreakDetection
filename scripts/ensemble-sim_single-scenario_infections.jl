9cfd287e7d80f6f94dff95ae9be20d275d115232
Callum Arnold <24391445+arnold-c@users.noreply.github.com>
2023-09-07 12:19:13 -0400

    Update single ensemble scenario spec to use the new EnsembleSpecification constructor that accepts a dict of initial state proportions

--- scripts/ensemble-sim_single-scenario_infections.jl
+++ scripts/ensemble-sim_single-scenario_infections.jl
@@ -10,9 +10,14 @@ include("ensemble-sim.jl")
 ensemble_spec = EnsembleSpecification(
     ("seasonal-infectivity-import", "tau-leaping"),
     500_000,
-    0.88,
+    Dict(
+        :s_prop => 0.1,
+        :e_prop => 0.01,
+        :i_prop => 0.01,
+        :r_prop => 0.88,
+    ),
     1_000,
-    20,
+    10,
     0.2,
     ensemble_time_p,
 )
@@ -25,7 +30,9 @@ ensemble_quants = get_ensemble_file(
     "95", ensemble_spec
 )
 
-@unpack ensemble_seir_arr, ensemble_jump_arr, ensemble_change_arr, ensemble_dynamics_p, ensemble_param_dict = ensemble_sol
+@unpack ensemble_seir_arr,
+ensemble_jump_arr, ensemble_change_arr, ensemble_dynamics_p,
+ensemble_param_dict = ensemble_sol
 @unpack ensemble_seir_summary, caption = ensemble_quants
 @unpack time_p = ensemble_param_dict
 
