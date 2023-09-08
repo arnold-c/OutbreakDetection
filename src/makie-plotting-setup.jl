ff5e954c6d9d5f27e989ed5344ee4c2b48833868
Callum Arnold <24391445+arnold-c@users.noreply.github.com>
2023-09-08 15:00:18 -0400

    Ignore makie plotting setup script
    Only there to quickly change settings when moving between work and home monitor setups

--- src/makie-plotting-setup.jl
+++
@@ -1,9 +0,0 @@
-using GLMakie
-using AlgebraOfGraphics
-using ColorSchemes
-
-set_aog_theme!()
-# Set depending on size of screen
-update_theme!(; resolution = (2200, 1300))
-#= update_theme!(; resolution = (850, 600)) =#
-GLMakie.activate!(; float = true)
