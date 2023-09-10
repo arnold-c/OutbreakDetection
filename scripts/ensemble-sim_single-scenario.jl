ef782b664e073bca3741b97d7143db85068c00b8
Callum Arnold <24391445+arnold-c@users.noreply.github.com>
2023-09-09 23:03:59 -0400

    Move all single scenario plotting code to one script, and tidy up time params spec
    Reduce unnecessary imports and complexity

--- scripts/ensemble-sim_single-scenario.jl
+++ scripts/ensemble-sim_single-scenario.jl
@@ -7,33 +7,38 @@ using UnPack
 include("ensemble-diag-testing_scenarios.jl")
 
 #%%
-ensemble_single_scenario_spec = ScenarioSpecification(
-    EnsembleSpecification(
-        ("seasonal-infectivity-import", "tau-leaping"),
-        StateParameters(
-            500_000,
-            Dict(
-                :s_prop => 0.1,
-                :e_prop => 0.01,
-                :i_prop => 0.01,
-                :r_prop => 0.88,
+ensemble_single_scenario_spec =
+    let time_params = SimTimeParameters(;
+            tmin = 0.0, tmax = 365.0 * 100, tstep = 1.0
+        )
+        ScenarioSpecification(
+            EnsembleSpecification(
+                ("seasonal-infectivity-import", "tau-leaping"),
+                StateParameters(
+                    500_000,
+                    Dict(
+                        :s_prop => 0.1,
+                        :e_prop => 0.01,
+                        :i_prop => 0.01,
+                        :r_prop => 0.88,
+                    ),
+                ),
+                DynamicsParameters(500_000, 5, 0.2),
+                time_params,
+                1_000,
             ),
-        ),
-        DynamicsParameters(500_000, 10, 0.2),
-        SimTimeParameters(; tmin = 0.0, tmax = 365.0 * 100, tstep = 1.0),
-        1_000,
-    ),
-    OutbreakSpecification(5, 30, 500),
-    create_static_NoiseSpecification(
-        [10.0],
-        SimTimeParameters(; tmin = 0.0, tmax = 365.0 * 100, tstep = 1.0),
-        0.0,
-        0.1,
-        1_000,
-    ),
-    OutbreakDetectionSpecification(10, 7, 0.3, 0.3, 3),
-    IndividualTestSpecification(0.8, 0.8),
-)
+            OutbreakSpecification(5, 30, 500),
+            create_static_NoiseSpecification(
+                [10.0],
+                time_params,
+                0.0,
+                0.1,
+                1_000
+            ),
+            OutbreakDetectionSpecification(10, 7, 0.3, 0.3, 3),
+            IndividualTestSpecification(0.8, 0.8),
+        )
+    end
 
 #%%
 ensemble_single_scenario_sol = get_ensemble_file(
@@ -47,12 +52,3 @@ ensemble_single_scenario_quantiles = get_ensemble_file(
 ensemble_single_scenario_detection = get_scenario_file(
     "scenario", ensemble_single_scenario_spec
 )
-
-#%%
-@unpack ensemble_seir_arr,
-dynamics_parameters,
-time_parameters = ensemble_single_scenario_sol
-
-@unpack ensemble_seir_summary, caption = ensemble_single_scenario_quantiles
-
-@unpack OT_Chars, incarr, testarr = ensemble_single_scenario_detection
--- scripts/ensemble-sim_single-scenario_infections-plots.jl
+++ scripts/ensemble-sim_single-scenario_infections-plots.jl
@@ -9,24 +9,84 @@ include("ensemble-sim_single-scenario.jl")
 include(srcdir("makie-plotting-setup.jl"))
 
 #%%
-outbreakcols = [ColorSchemes.magma[i] for i in (200, 20)]
-
-detect_outbreak_plot(
-    incarr,
-    ensemble_seir_arr,
-    ensemble_single_scenario_spec.ensemble_specification.time_parameters;
-    colormap = outbreakcols,
-    xlims = (90, 100),
-    ylims_inc = (0, 150),
-    ylims_periodsum = (0, 1000),
-)
-
-#%%
 create_sir_quantiles_plot(
-    ensemble_seir_summary;
+    ensemble_single_scenario_quantiles["ensemble_seir_summary"];
     labels = seir_state_labels,
     colors = seircolors,
     annual = true,
     caption = caption,
     timeparams = ensemble_single_scenario_spec.ensemble_specification.time_parameters,
+)
+
+#%%
+outbreakcols = [ColorSchemes.magma[i] for i in (200, 20)]
+
+detect_outbreak_plot(
+    ensemble_single_scenario_detection["incarr"],
+    ensemble_single_scenario_sol["ensemble_seir_arr"],
+    ensemble_single_scenario_spec.ensemble_specification.time_parameters;
+    colormap = outbreakcols,
+    # xlims = (90, 100),
+    # ylims_inc = (0, 150),
+    # ylims_periodsum = (0, 1000),
+)
+
+#%%
+visualize_ensemble_noise(
+    ensemble_single_scenario_spec.noise_specification.noise_array,
+    ensemble_single_scenario_spec.noise_specification.time_parameters,
+)
+
+#%%
+incidence_testing_plot(
+    ensemble_single_scenario_detection["incarr"],
+    ensemble_single_scenario_detection["testarr"],
+    ensemble_single_scenario_spec.ensemble_specification.time_parameters,
+    ensemble_single_scenario_spec.outbreak_detection_specification.detection_threshold;
+    sim = 1,
+)
+
+#%%
+testing_plot(
+    ensemble_single_scenario_detection["testarr"],
+    ensemble_single_scenario_spec.ensemble_specification.time_parameters,
+)
+
+#%%
+ensemble_outbreak_distribution_plot(
+    ensemble_single_scenario_detection["testarr"],
+    ensemble_single_scenario_detection["incarr"],
+)
+
+#%%
+ensemble_OTChars_plot(
+    ensemble_single_scenario_detection["OT_chars"],
+    :sensitivity,
+    :specificity;
+    bins = 0.0:0.01:1.01,
+    char1_label = "Sensitivity",
+    char2_label = "Specificity",
+    xlabel = "Proportion",
+    legendlabel = "Characterstic",
+)
+
+#%%
+ensemble_OTChars_plot(
+    ensemble_single_scenario_detection["OT_chars"],
+    :noutbreaks,
+    :ndetectoutbreaks,
+)
+
+#%%
+ensemble_OTChars_plot(
+    ensemble_single_scenario_detection["OT_chars"],
+    :ppv,
+    :npv;
+    bins = 0.0:0.01:1.01,
+    char1_label = "PPV",
+    char2_label = "NPV",
+    char1_color = :green,
+    char2_color = :purple,
+    xlabel = "Proportion",
+    legendlabel = "Characterstic",
 )
