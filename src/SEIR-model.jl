249192eebd67e0e00e18abe2213c920a3d40e835
Callum Arnold <24391445+arnold-c@users.noreply.github.com>
2023-09-14 15:15:16 -0400

    Create a long version of the SEIR model to test how fast it is compared to wide format
    Seems to be about the same speed, which is good for using later in the incidence array creation

--- src/SEIR-model.jl
+++ src/SEIR-model.jl
@@ -23,6 +23,25 @@ function calculate_beta_amp(beta_mean, beta_force, t)
     return beta_mean * (1 + beta_force * cos(2pi * t / 365))
 end
 
+function seir_mod_long(
+    states,
+    dynamics_params,
+    time_params;
+    retbetaarr = false,
+    type = "stoch",
+    seed = 1234,
+)
+    state_arr, change_arr, jump_arr = seir_mod(
+        states, dynamics_params, time_params; retbetaarr = retbetaarr, type = type, seed = seed
+    )
+
+    state_long = permutedims(state_arr, (2, 1))
+    change_long = permutedims(change_arr, (2, 1))
+    jump_long = permutedims(jump_arr, (2, 1))
+
+    return state_long, change_long, jump_long
+end
+
 """
     seir_mod(states, dynamics_params, trange; retbetaarr = false, type = "stoch")
 
@@ -166,7 +185,8 @@ function seir_mod_loop!(
 
     rates = [
         infec_rate, latent_rate, recov_rate, birth_rate, S_death_rate,
-        E_death_rate, I_death_rate, R_death_rate, import_rate]
+        E_death_rate, I_death_rate, R_death_rate, import_rate
+    ]
 
     # Calculate the number of jumps for each event
     if type == "stoch"
@@ -193,7 +213,7 @@ function seir_mod_loop!(
     change_arr[4, j] = jump_arr[3, j] - jump_arr[8, j]
     change_arr[5, j] = sum(change_arr[1:4, j])
 
-    # Check that the change in each state does not result in a negative state, 
+    # Check that the change in each state does not result in a negative state,
     # and if it is, set the change to the negative of the current state (i.e.
     # set the new state value to 0)
     for state in axes(state_arr, 1)
@@ -203,6 +223,101 @@ function seir_mod_loop!(
     end
 
     @. state_arr[:, j] = state_arr[:, j - 1] + change_arr[:, j]
+
+    return nothing
+end
+
+
+function seir_mod_long!(
+    state_arr, change_arr, jump_arr, states, dynamics_params, time_params;
+    type = "stoch",
+    seed = 1234,
+)
+    Random.seed!(seed)
+
+    for (i, t) in pairs(time_params.trange)
+        if i == 1
+            state_arr[i, :] = states
+            continue
+        end
+
+        seir_mod_long_loop!(
+            state_arr, change_arr, jump_arr, i, dynamics_params, time_params, t;
+            type = type,
+        )
+    end
+
+    return nothing
+end
+
+function seir_mod_long_loop!(
+    state_arr, change_arr, jump_arr, i, dynamics_params, time_params, t;
+    type = type
+)
+    # Unpack the state variables for easier use
+    S = state_arr[i - 1, 1]
+    E = state_arr[i - 1, 2]
+    I = state_arr[i - 1, 3]
+    R = state_arr[i - 1, 4]
+    N = state_arr[i - 1, 5]
+
+    # Unpack the parameters for easier use
+    @unpack tstep = time_params
+    @unpack beta_mean, beta_force, sigma, gamma, mu, epsilon, R_0 =
+        dynamics_params
+    beta_t = calculate_beta_amp(beta_mean, beta_force, t)
+
+    # Calculate the rates of each event
+    infec_rate = beta_t * S * I        # 1
+    latent_rate = sigma * E             # 2
+    recov_rate = gamma * I              # 3
+    birth_rate = mu * N              # 4
+    S_death_rate = mu * S            # 5
+    E_death_rate = mu * E            # 6
+    I_death_rate = mu * I            # 7
+    R_death_rate = mu * R            # 8
+    import_rate = epsilon * N / R_0        # 9
+
+    rates = [
+        infec_rate, latent_rate, recov_rate, birth_rate, S_death_rate,
+        E_death_rate, I_death_rate, R_death_rate, import_rate
+    ]
+
+    # Calculate the number of jumps for each event
+    if type == "stoch"
+
+        jump_arr[i, :] = map(
+            r -> rand(Poisson(r * tstep)),
+            rates,
+        )
+    elseif type == "det"
+        jump_arr[i, :] = map(
+            r -> r * tstep,
+            rates,
+        )
+    else
+        return ("Type must be stoch or det")
+    end
+
+    # Calculate the change in each state
+    change_arr[i, 1] =
+        jump_arr[i, 4] - jump_arr[i, 1] - jump_arr[i, 5] - jump_arr[i, 9]
+    change_arr[i, 2] =
+        jump_arr[i, 1] - jump_arr[i, 2] - jump_arr[i, 6] + jump_arr[i, 9]
+    change_arr[i, 3] = jump_arr[i, 2] - jump_arr[i, 3] - jump_arr[i, 7]
+    change_arr[i, 4] = jump_arr[i, 3] - jump_arr[i, 8]
+    change_arr[i, 5] = sum(change_arr[i, 1:4])
+
+    # Check that the change in each state does not result in a negative state,
+    # and if it is, set the change to the negative of the current state (i.e.
+    # set the new state value to 0)
+    for state in axes(state_arr, 2)
+        if state_arr[i - 1, state] + change_arr[i, state] < 0
+            change_arr[i, state] = -state_arr[i - 1, state]
+        end
+    end
+
+    @. state_arr[i, :] = state_arr[i - 1, :] + change_arr[i, :]
 
     return nothing
 end
