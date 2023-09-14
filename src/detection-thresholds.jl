08c8a46a184e7d0d2af2059703ca3871978bc773
Callum Arnold <24391445+arnold-c@users.noreply.github.com>
2023-09-14 00:34:48 -0400

    Remove views of daily incidence and pass in abovethreshold vector to be mutated in place in create_inc_infec_arr! functions
    Much faster without copying values (can be sourced from a different source if necessary), and mutating a vector results in far fewer allocations

--- src/detection-thresholds.jl
+++ src/detection-thresholds.jl
@@ -10,14 +10,13 @@ using UnPack
 function create_inc_infec_arr(
     ensemble_jump_arr, outbreak_specification::OutbreakSpecification
 )
-    incarr = create_inc_infec_arr(
+    return create_inc_infec_arr(
         ensemble_jump_arr,
         outbreak_specification.outbreak_threshold,
         outbreak_specification.minimum_outbreak_duration,
         outbreak_specification.minimum_outbreak_size,
     )
 
-    return incarr
 end
 
 function create_inc_infec_arr(
@@ -27,19 +26,22 @@ function create_inc_infec_arr(
         Int64, size(ensemble_jump_arr, 2), 4, size(ensemble_jump_arr, 3)
     )
 
+    abovethreshold = zeros(Int64, size(ensemble_jump_arr, 2))
+
     create_inc_infec_arr!(
         incarr,
         ensemble_jump_arr,
         outbreakthreshold,
         minoutbreakdur,
         minoutbreaksize,
+        abovethreshold
     )
 
     return incarr
 end
 
 function create_inc_infec_arr!(
-    incarr, ensemble_jump_arr, outbreak_specification::OutbreakSpecification
+    incarr, ensemble_jump_arr, outbreak_specification::OutbreakSpecification, abovethreshold
 )
     create_inc_infec_arr!(
         incarr,
@@ -47,22 +49,21 @@ function create_inc_infec_arr!(
         outbreak_specification.outbreak_threshold,
         outbreak_specification.minimum_outbreak_duration,
         outbreak_specification.minimum_outbreak_size,
+        abovethreshold
     )
     return nothing
 end
 
 function create_inc_infec_arr!(
-    incarr, ensemblejumparr, outbreakthreshold, minoutbreakdur, minoutbreaksize
+    incarr, ensemblejumparr, outbreakthreshold, minoutbreakdur, minoutbreaksize, abovethreshold
 )
-    incarr[:, 2, :] .= @view(ensemblejumparr[1, :, :]) .>= outbreakthreshold
-
     for sim in axes(ensemblejumparr, 3)
-        above5rle = rle(@view(incarr[:, 2, sim]))
+        abovethreshold .= view(ensemblejumparr, 1, :, sim) .>= outbreakthreshold
 
-        ## Calculate upper and lower indices of consecutive days of infection
-        above5lowers, above5uppers = calculate_outbreak_thresholds(above5rle)
+        # Calculate upper and lower indices of consecutive days of infection
+        abovethresholdlowers, abovethresholduppers = calculate_outbreak_thresholds(abovethreshold)
 
-        for (lower, upper) in zip(above5lowers, above5uppers)
+        for (lower, upper) in zip(abovethresholdlowers, abovethresholduppers)
             calculate_period_sum!(incarr, ensemblejumparr, lower, upper, sim)
             classify_outbreak!(
                 incarr, lower, upper, sim, minoutbreakdur, minoutbreaksize
@@ -75,7 +76,7 @@ function calculate_outbreak_thresholds(outbreakrle)
     # Calculate upper and lower indices of consecutive days of infection
     outbreakaccum = accumulate(+, outbreakrle[2])
     upperbound_indices = findall(isequal(1), outbreakrle[1])
-    outbreakuppers = @view(outbreakaccum[upperbound_indices])
+    outbreakuppers = view(outbreakaccum,upperbound_indices)
     outbreaklowers = map(
         x -> x - 1 == 0 ? 1 : outbreakaccum[x - 1] + 1, upperbound_indices
     )
@@ -85,7 +86,7 @@ end
 
 function calculate_period_sum!(incarr, jumparr, lower, upper, sim)
     return incarr[lower:upper, 3, sim] .= sum(
-        @view(jumparr[1, lower:upper, sim])
+        view(jumparr,1, lower:upper, sim)
     )
 end
 
@@ -94,6 +95,7 @@ function classify_outbreak!(
 )
     if lower - upper >= minoutbreakdur &&
         incarr[lower, 3, sim] >= minoutbreaksize
+
         incarr[lower:upper, 4, sim] .= 1
     end
 end
