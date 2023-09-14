6037fe833ec60fb2628f3a9dbe43397f2176e3e0
Callum Arnold <24391445+arnold-c@users.noreply.github.com>
2023-09-14 17:57:53 -0400

    Speed up SEIR long model by 100% using simd and views

--- src/SEIR-model.jl
+++ src/SEIR-model.jl
@@ -229,7 +229,7 @@ end
 
 
 function seir_mod_long!(
-    state_arr, change_arr, jump_arr, states, dynamics_params, time_params;
+    state_arr, change_arr, jump_arr, states, rates, dynamics_params, time_params;
     type = "stoch",
     seed = 1234,
 )
@@ -237,12 +237,12 @@ function seir_mod_long!(
 
     for (i, t) in pairs(time_params.trange)
         if i == 1
-            state_arr[i, :] = states
+            state_arr[i, :] .= states
             continue
         end
 
         seir_mod_long_loop!(
-            state_arr, change_arr, jump_arr, i, dynamics_params, time_params, t;
+            state_arr, change_arr, jump_arr, rates, i, dynamics_params, time_params, t;
             type = type,
         )
     end
@@ -251,73 +251,55 @@ function seir_mod_long!(
 end
 
 function seir_mod_long_loop!(
-    state_arr, change_arr, jump_arr, i, dynamics_params, time_params, t;
-    type = type
+    state_arr, change_arr, jump_arr, rates, i, dynamics_params, time_params, t;
+    type = type,
 )
-    # Unpack the state variables for easier use
-    S = state_arr[i - 1, 1]
-    E = state_arr[i - 1, 2]
-    I = state_arr[i - 1, 3]
-    R = state_arr[i - 1, 4]
-    N = state_arr[i - 1, 5]
-
-    # Unpack the parameters for easier use
-    @unpack tstep = time_params
-    @unpack beta_mean, beta_force, sigma, gamma, mu, epsilon, R_0 =
-        dynamics_params
-    beta_t = calculate_beta_amp(beta_mean, beta_force, t)
+    # Calculate beta for current time
+    beta_t = calculate_beta_amp(dynamics_params.beta_mean, dynamics_params.beta_force, t)
 
     # Calculate the rates of each event
-    infec_rate = beta_t * S * I        # 1
-    latent_rate = sigma * E             # 2
-    recov_rate = gamma * I              # 3
-    birth_rate = mu * N              # 4
-    S_death_rate = mu * S            # 5
-    E_death_rate = mu * E            # 6
-    I_death_rate = mu * I            # 7
-    R_death_rate = mu * R            # 8
-    import_rate = epsilon * N / R_0        # 9
-
-    rates = [
-        infec_rate, latent_rate, recov_rate, birth_rate, S_death_rate,
-        E_death_rate, I_death_rate, R_death_rate, import_rate
-    ]
+    @views rates[1] = beta_t .* state_arr[i - 1, 1] .* state_arr[i - 1, 2]
+    @views rates[2] = dynamics_params.sigma .* state_arr[i - 1, 2]
+    @views rates[3] = dynamics_params.gamma .* state_arr[i - 1, 3]
+    @views rates[4] = dynamics_params.mu .* state_arr[i - 1, 5]
+    @views rates[5] = dynamics_params.mu .* state_arr[i - 1, 1]
+    @views rates[6] = dynamics_params.mu .* state_arr[i - 1, 3]
+    @views rates[7] = dynamics_params.mu .* state_arr[i - 1, 2]
+    @views rates[8] = dynamics_params.mu .* state_arr[i - 1, 4]
+    @views rates[9] = dynamics_params.epsilon .* state_arr[i - 1, 5] ./ dynamics_params.R_0
 
     # Calculate the number of jumps for each event
     if type == "stoch"
-
-        jump_arr[i, :] = map(
-            r -> rand(Poisson(r * tstep)),
-            rates,
-        )
+        @simd for r in eachindex(rates)
+            jump_arr[i, r] = rand(Poisson(rates[r] * time_params.tstep))
+        end
     elseif type == "det"
-        jump_arr[i, :] = map(
-            r -> r * tstep,
-            rates,
-        )
+        @simd for r in eachindex(rates)
+            jump_arr[i, r] = r * time_params.tstep
+        end
     else
         return ("Type must be stoch or det")
     end
 
     # Calculate the change in each state
-    change_arr[i, 1] =
-        jump_arr[i, 4] - jump_arr[i, 1] - jump_arr[i, 5] - jump_arr[i, 9]
-    change_arr[i, 2] =
-        jump_arr[i, 1] - jump_arr[i, 2] - jump_arr[i, 6] + jump_arr[i, 9]
-    change_arr[i, 3] = jump_arr[i, 2] - jump_arr[i, 3] - jump_arr[i, 7]
-    change_arr[i, 4] = jump_arr[i, 3] - jump_arr[i, 8]
-    change_arr[i, 5] = sum(change_arr[i, 1:4])
+    @views change_arr[i, 1] = jump_arr[i, 4] - jump_arr[i, 1] - jump_arr[i, 5] - jump_arr[i, 9]
+    @views change_arr[i, 2] = jump_arr[i, 1] - jump_arr[i, 2] - jump_arr[i, 6] + jump_arr[i, 9]
+    @views change_arr[i, 3] = jump_arr[i, 2] - jump_arr[i, 3] - jump_arr[i, 7]
+    @views change_arr[i, 4] = jump_arr[i, 3] - jump_arr[i, 8]
+    @views change_arr[i, 5] = sum(change_arr[i, 1:4])
 
     # Check that the change in each state does not result in a negative state,
     # and if it is, set the change to the negative of the current state (i.e.
     # set the new state value to 0)
     for state in axes(state_arr, 2)
-        if state_arr[i - 1, state] + change_arr[i, state] < 0
-            change_arr[i, state] = -state_arr[i - 1, state]
+        @views if state_arr[i - 1, state] + change_arr[i, state] < 0
+            @views change_arr[i, state] = -state_arr[i - 1, state]
         end
     end
 
-    @. state_arr[i, :] = state_arr[i - 1, :] + change_arr[i, :]
+    previous_states = @view(state_arr[i - 1, :])
+    current_changes = @view(change_arr[i, :])
+    @turbo @. state_arr[i, :] = previous_states + current_changes
 
     return nothing
 end
