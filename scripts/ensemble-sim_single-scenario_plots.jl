fd0fcfb8ce61703b06254da3c704edab2c72d7cd
Callum Arnold <24391445+arnold-c@users.noreply.github.com>
2023-09-18 16:37:51 -0400

    Rename ensemble single scenario plotting script and save plots to file

--- scripts/ensemble-sim_single-scenario_plots.jl
+++ scripts/ensemble-sim_single-scenario_plots.jl
@@ -9,7 +9,7 @@ include("ensemble-sim_single-scenario.jl")
 include(srcdir("makie-plotting-setup.jl"))
 
 #%%
-create_sir_quantiles_plot(
+ensemble_single_scenario_quantiles_plot = create_sir_quantiles_plot(
     ensemble_single_scenario_quantiles["ensemble_seir_summary"];
     labels = seir_state_labels,
     colors = seircolors,
@@ -18,11 +18,14 @@ create_sir_quantiles_plot(
     timeparams = ensemble_single_scenario_spec.ensemble_specification.time_parameters,
 )
 
+save(plotsdir("ensemble-sim_single-scenario_quantiles.png"), ensemble_single_scenario_quantiles_plot)
+
 #%%
 outbreakcols = [ColorSchemes.magma[i] for i in (200, 20)]
 
-detect_outbreak_plot(
-    ensemble_single_scenario_detection["incarr"],
+ensemble_single_scenario_detect_outbreak_plot = detect_outbreak_plot(
+    ensemble_incarr,
+    # ensemble_single_scenario_detection["incarr"],
     ensemble_single_scenario_sol["ensemble_seir_arr"],
     ensemble_single_scenario_spec.ensemble_specification.time_parameters;
     colormap = outbreakcols,
@@ -31,20 +34,26 @@ detect_outbreak_plot(
     # ylims_periodsum = (0, 1000),
 )
 
+save(plotsdir("ensemble-sim_single-scenario_detect-outbreak.png"), ensemble_single_scenario_detect_outbreak_plot)
+
 #%%
-visualize_ensemble_noise(
+ensemble_single_scenario_noise_plot = visualize_ensemble_noise(
     ensemble_single_scenario_spec.noise_specification.noise_array,
     ensemble_single_scenario_spec.noise_specification.time_parameters,
 )
 
+save(plotsdir("ensemble-sim_single-scenario_noise.png"), ensemble_single_scenario_noise_plot)
+
 #%%
-incidence_testing_plot(
+ensemble_single_scenario_incidence_testing_plot = incidence_testing_plot(
     ensemble_single_scenario_detection["incarr"],
     ensemble_single_scenario_detection["testarr"],
     ensemble_single_scenario_spec.ensemble_specification.time_parameters,
     ensemble_single_scenario_spec.outbreak_detection_specification.detection_threshold;
     sim = 1,
 )
+
+save(plotsdir("ensemble-sim_single-scenario_incidence-testing.png"), ensemble_single_scenario_incidence_testing_plot)
 
 #%%
 testing_plot(
