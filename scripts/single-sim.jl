cab805b85cf5ecd6879c162c73a9ef9d3bb39e7f
Callum Arnold <24391445+arnold-c@users.noreply.github.com>
2023-09-18 17:43:52 -0400

    Remove wide SEIR model as no longer required for comparison

--- scripts/single-sim.jl
+++ scripts/single-sim.jl
@@ -15,21 +15,6 @@ seir_array, change_array, jump_array, beta_arr = seir_mod(
     singlesim_states_p.init_states, singlesim_dynamics_p, singlesim_time_p;
     type = "det", seed = 1234,
 );
-seir_wide_array' == seir_array
-
-seir_wide_array, change_wide_array, jump_wide_array, beta_wide_arr = seir_wide_mod(
-    singlesim_states_p.init_states, singlesim_dynamics_p, singlesim_time_p;
-    retbetaarr = true, type = "det", seed = 1234,
-);
-
-
-change_array[1:10, :]
-change_wide_array'[1:10, :]
-
-
-compare_jumps = DataFrame("time" => trange, "long" => jump_array[:, 1], "wide" => jump_wide_array[1, :])
-
-@subset(compare_jumps, :long .!= :wide)
 
 #%%
 seir_df = create_sir_df(
