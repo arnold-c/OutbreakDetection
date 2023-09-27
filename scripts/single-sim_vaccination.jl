09e0db771e07a93f49e38f48f14e0f605550cf61
Callum Arnold <24391445+arnold-c@users.noreply.github.com>
2023-09-27 12:28:02 -0400

    Compare vaccination and non vaccination implementations
    Should be equivalent when vaccination rates are set to 0 in the vaccination implementation

--- scripts/single-sim_vaccination.jl
+++ scripts/single-sim_vaccination.jl
@@ -11,6 +11,7 @@ include(srcdir("makie-plotting-setup.jl"))
 
 #%%
 @unpack singlesim_states_p, singlesim_time_p, singlesim_dynamics_p = load("data/singlesim/single-sim_setup.jld2")
+@unpack seir_array = load("data/singlesim/single-sim_arrays.jld2")
 
 #%%
 seirv_array, changev_array, jumpv_array, betav_arr = seirv_mod(
@@ -24,11 +25,13 @@ seirv_array, changev_array, jumpv_array, betav_arr = seirv_mod(
         singlesim_dynamics_p.annual_births_per_k,
         singlesim_dynamics_p.epsilon,
         singlesim_dynamics_p.R_0,
-        0.8
+        0.0
     ),
     singlesim_time_p; type = "stoch",
     seed = 1234,
 );
+
+seir_array == seirv_array
 
 #%%
 seirv_df = create_sir_df(
@@ -38,6 +41,14 @@ seirv_df = create_sir_df(
 #%%
 singlesim_vaccination_timeseries_plot = draw_sir_plot(
     seirv_df;
+    annual = true,
+    colors = seircolors,
+    labels = seir_state_labels
+)
+
+#%%
+draw_sir_plot(
+    create_sir_df(seir_array, singlesim_time_p.trange, [:S, :E, :I, :R, :N]);
     annual = true,
     colors = seircolors,
     labels = seir_state_labels
