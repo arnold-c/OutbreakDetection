968187efb21479303d8187a6e59f519dfe2d2b7e
Callum Arnold <24391445+arnold-c@users.noreply.github.com>
2023-09-28 10:55:21 -0400

    Update poisson noise array to accept the noise specification and incidence array
    Want to produce an array with an identical length to the incidence array, and need to calculate the mean of each timeseries.

--- src/noise-functions.jl
+++ src/noise-functions.jl
@@ -2,26 +2,28 @@
 #
 # export create_noise_arr, create_noise_arr!
 
-using DifferentialEquations
-using FLoops
-
 # include("ensemble-functions.jl")
 # using .EnsembleFunctions
 
-function create_poisson_noise_arr(
-    noise_mean, timeparams::SimTimeParameters, nsims
-)
-    noise_arr = zeros(Int64, timeparams.tlength, 1, nsims)
+function create_poisson_noise_arr(incarr, noise_spec::NoiseSpecification; seed = 1234)
+    noise_arr = zeros(Int64, size(incarr, 1), 1, size(incarr, 3))
 
-    create_poisson_noise_arr!(noise_arr, noise_mean, timeparams)
+    create_poisson_noise_arr!(
+        noise_arr, incarr, noise_spec.noise_mean_scaling; seed = seed
+    )
 
     return noise_arr
 end
 
-function create_poisson_noise_arr!(noise_arr, noise_mean, timeparams)
-    Random.seed!(1234)
-    for sim in axes(noise_arr, 3)
-        noise_arr[:, 1, sim] .= rand(Poisson(noise_mean), timeparams.tlength)
+function create_poisson_noise_arr!(
+    noise_arr, incarr, noise_mean_scaling; seed = 1234
+)
+    Random.seed!(seed)
+    @inbounds for sim in axes(incarr, 3)
+        @views noise_arr[:, 1, sim] .= rand(
+            Poisson(noise_mean_scaling * mean(incarr[:, 1, sim])),
+            size(incarr, 1),
+        )
     end
 end
 
