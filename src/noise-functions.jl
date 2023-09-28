b19213dc1b186be1c311bebe1f8fc24a65c78fc8
Callum Arnold <24391445+arnold-c@users.noreply.github.com>
2023-09-28 00:03:15 -0400

    Create Poisson noise functions

--- src/noise-functions.jl
+++ src/noise-functions.jl
@@ -8,6 +8,22 @@ using FLoops
 # include("ensemble-functions.jl")
 # using .EnsembleFunctions
 
+function create_poisson_noise_arr(
+    noise_mean, timeparams::SimTimeParameters, nsims
+)
+    noise_arr = zeros(Int64, timeparams.tlength, 1, nsims)
+
+    create_poisson_noise_arr!(noise_arr, noise_mean, timeparams)
+
+    return noise_arr
+end
+
+function create_poisson_noise_arr!(noise_arr, noise_mean, timeparams)
+    Random.seed!(1234)
+    for sim in axes(noise_arr, 3)
+        noise_arr[:, 1, sim] .= rand(Poisson(noise_mean), timeparams.tlength)
+    end
+end
 
 function create_static_noise_arr(
     init_noise,
