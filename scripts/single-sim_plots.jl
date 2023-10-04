fb736d61beff5b3e7e0c424d3fb5274f7a554107
Callum Arnold <24391445+arnold-c@users.noreply.github.com>
2023-10-04 17:01:34 -0400

    Remove old single sim jump and change plots

--- scripts/single-sim_plots.jl
+++ scripts/single-sim_plots.jl
@@ -28,64 +28,9 @@ singlesim_timeseries_plot = draw_sir_plot(
 save(plotsdir("singlesim/single-sim_timeseries.png"), singlesim_timeseries_plot)
 
 #%%
-singlesim_jump_plot = @chain DataFrame(Tables.table(jump_array)) begin
-    hcat(trange, _)
-    rename!([
-        "time",
-        "Infect",
-        "Susceptible Births",
-        "S death",
-        "R death",
-        "Import",
-        "Protected Births",
-        "Latent",
-        "E death",
-        "Recovery",
-        "I death"
-    ])
-    stack(_, Not("time"); variable_name = :Jump, value_name = :Number)
-    data(_) *
-    mapping(
-        :time => (t -> t / 365) => "Time (years)",
-        :Number;
-        color = :Jump,
-        layout = :Jump,
-    ) *
-    visual(Lines; linewidth = 1)
-    draw(;
-        facet = (; linkyaxes = :none), axis = (; limits = (nothing, nothing))
-    )
-end
-
-save(plotsdir("singlesim/single-sim_jumps.png"), singlesim_jump_plot)
-
-#%%
-change_labels = ["dS", "dE", "dI", "dR", "dN"]
-singlesim_change_plot = @chain DataFrame(Tables.table(change_array)) begin
-    hcat(trange, _)
-    rename!([
-        "time", change_labels...
-    ])
-    stack(_, Not("time"); variable_name = :Change, value_name = :Number)
-    data(_) *
-    mapping(
-        :time => (t -> t / 365) => "Time (years)", :Number;
-        color = :Change => sorter(change_labels...), layout = :Change,
-    ) *
-    visual(Lines; linewidth = 1)
-    draw(;
-        facet = (; linkyaxes = :none),
-        palettes = (; color = seircolors),
-        axis = (; limits = ((0, 100), nothing)),
-    )
-end
-
-save(plotsdir("singlesim/single-sim_changes.png"), singlesim_change_plot)
-
-#%%
 singlesim_si_state_space_plot = @chain DataFrame(Tables.table(seir_array)) begin
     hcat(trange, _)
-    rename!(["time", seir_state_labels...])
+    rename!(["time", ["S", "E", "I", "R", "N", "incidence"]...])
     data(_) *
     mapping(:I, :S; color = :time) *
     visual(Lines)
@@ -98,7 +43,7 @@ save(
 )
 
 #%%
-singlesim_beta_plot = @chain DataFrame(Tables.table(beta_arr)) begin
+singlesim_beta_plot = @chain DataFrame(Tables.table(beta_vec)) begin
     hcat(trange, _)
     rename!([:time, :beta_t])
     stack(_, Not("time"); variable_name = :beta, value_name = :Number)
