5b4622d77643a80d71f65d1fab9bbeff11e97404
Callum Arnold <24391445+arnold-c@users.noreply.github.com>
2023-10-25 17:59:13 -0400

    Test code to calculate the outbreak thresholds - output thresholds to new array
    Currently breaks the later plots, and using arrays seems less ideal that using NamedTuples, but seems to be much quicker as fewer allocations

--- src/detection-thresholds.jl
+++ src/detection-thresholds.jl
@@ -15,8 +15,8 @@ function create_inc_infec_arr(
         Int64, size(ensemble_inc_vecs, 1), 4, size(ensemble_inc_vecs, 2)
     )
 
-    ensemble_thresholds_vec = repeat(
-        [Vector{NamedTuple}(undef, 0)], size(ensemble_inc_vecs, 2)
+    ensemble_thresholds_vec = Vector{Array{Int64, 2}}(
+        undef, size(ensemble_inc_vecs, 2)
     )
 
     create_inc_infec_arr!(
@@ -37,6 +37,8 @@ function create_inc_infec_arr!(
     minoutbreaksize,
 )
     @inbounds for sim in axes(ensemble_inc_vecs, 2)
+        all_thresholds_arr = zeros(Int64, size(ensemble_inc_vecs, 1), 4)
+
         convert_svec_to_matrix!(
             @view(ensemble_inc_arr[:, 1, sim]),
             @view(ensemble_inc_vecs[:, sim])
@@ -55,20 +57,41 @@ function create_inc_infec_arr!(
             outbreak_thresholds.lowers,
             outbreak_thresholds.uppers
         )
-            calculate_period_sum!(
-                @view(ensemble_inc_arr[lower:upper, 3, sim]),
-                @view(ensemble_inc_arr[lower:upper, 1, sim])
-            )
-            classify_outbreak!(
-                @view(ensemble_inc_arr[lower:upper, 4, sim]),
-                ensemble_thresholds_vec[sim],
-                ensemble_inc_arr[lower, 3, sim],
-                upper,
-                lower,
-                minoutbreakdur,
-                minoutbreaksize,
-            )
+            all_thresholds_arr[lower, 1] = lower
+            all_thresholds_arr[lower, 2] = upper
+
+            period_sum = sum(@view(ensemble_inc_arr[lower:upper, 1, sim]))
+
+            all_thresholds_arr[lower, 3] = period_sum
+
+            if upper - lower >= minoutbreakdur &&
+                period_sum >= minoutbreaksize
+                outbreak_class = 1
+            else
+                outbreak_class = 0
+            end
+
+            all_thresholds_arr[lower, 4] = outbreak_class
+
+            # calculate_period_sum!(
+            #     @view(ensemble_inc_arr[lower:upper, 3, sim]),
+            #     @view(ensemble_inc_arr[lower:upper, 1, sim])
+            # )
+            # classify_outbreak!(
+            #     @view(ensemble_inc_arr[lower:upper, 4, sim]),
+            #     @view(all_thresholds_arr[lower, :]),
+            #     ensemble_inc_arr[lower, 3, sim],
+            #     upper,
+            #     lower,
+            #     minoutbreakdur,
+            #     minoutbreaksize,
+            # )
         end
+
+        ensemble_thresholds_vec[sim] = @view(all_thresholds_arr[
+            (all_thresholds_arr[:, 1] .!= 0), :,
+        ])
+
     end
     return nothing
 end
@@ -91,14 +114,18 @@ function calculate_period_sum!(outvec, incvec)
 end
 
 function classify_outbreak!(
-    outvec, outbreak_thresholds_vec, periodsumvec, upper_time, lower_time,
+    outvec, thresholds_vec, periodsumvec, upper_time, lower_time,
     minoutbreakdur,
     minoutbreaksize,
 )
     if upper_time - lower_time >= minoutbreakdur &&
         periodsumvec >= minoutbreaksize
         @inbounds outvec .= 1
-        push!(outbreak_thresholds_vec, (lower = lower_time, upper = upper_time))
+        thresholds_vec[1] = lower_time
+        thresholds_vec[2] = upper_time
+
+        # BUG: Can't use push! as causes a data race issue
+        # push!(outbreak_thresholds_vec, (lower = lower_time, upper = upper_time))
     end
     return nothing
 end
