e4ca78334d17e406accff55210845b8d03b8ef6b
Callum Arnold <24391445+arnold-c@users.noreply.github.com>
2023-10-26 02:14:30 -0400

    Write a quick test for the detection delay function

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,8 @@
 using DrWatson, Test
 @quickactivate "OutbreakDetection"
 
+using OutbreakDetection
+
 # Here you include files using `srcdir`
 # include(srcdir("file.jl"))
 
@@ -12,6 +14,23 @@ ti = time()
     @test 1 == 1
 end
 
+@testset "Detection tests" begin
+    @testset "Delay detection" begin
+        @test begin
+            outbreakbounds = [2 4; 10 60; 100 180; 300 340; 380 400]
+            detectionbounds = [5 15; 12 40; 50 80; 90 105; 110 160; 390 420]
+            calculate_outbreak_detection_delay(
+                outbreakbounds, detectionbounds
+            ) ==
+            [2 4 -10 -10 -10
+                10 60 12 40 2
+                100 180 110 160 10
+                300 340 -10 -10 -10
+                380 400 390 420 10]
+        end
+    end
+end
+
 ti = time() - ti
 println("\nTest took total time of:")
-println(round(ti / 60; digits=3), " minutes")
+println(round(ti / 60; digits = 3), " minutes")
