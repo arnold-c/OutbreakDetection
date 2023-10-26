9144d93f996fdf7f9b42e3b8b1fbe9d709eaa354
Callum Arnold <24391445+arnold-c@users.noreply.github.com>
2023-10-25 23:58:50 -0400

    Delete method for calculate_OutbreakThresholdChars that doesn't accept the outbreak bounds as an argument
    Produces innaccurate measures of number of true outbreaks as doesn't have information about the period sum

--- src/diag-testing-functions.jl
+++ src/diag-testing-functions.jl
@@ -215,25 +215,6 @@ function calculate_test_positivity(
     return outvec
 end
 
-function calculate_OutbreakThresholdChars(testarr, infecarr)
-    OT_chars = map(axes(infecarr, 3)) do sim
-        outbreakrle = rle(@view(infecarr[:, 3, sim]))
-        detectrle = rle(@view(testarr[:, 7, sim]))
-
-        OutbreakThresholdChars(
-            calculate_ot_characterstics(
-                @view(testarr[:, 7, sim]), @view(infecarr[:, 3, sim])
-            )...,
-            calculate_noutbreaks(outbreakrle),
-            calculate_noutbreaks(detectrle),
-            calculate_outbreak_thresholds(outbreakrle; ncols = 2),
-            calculate_outbreak_thresholds(detectrle; ncols = 2),
-        )
-    end
-
-    return StructArray(OT_chars)
-end
-
 function calculate_OutbreakThresholdChars(testarr, infecarr, thresholds_vec)
     OT_chars = map(axes(infecarr, 3)) do sim
         detectrle = rle(@view(testarr[:, 7, sim]))
