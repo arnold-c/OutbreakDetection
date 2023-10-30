0b169ac1a9394946c35c1be4e714b4a6bb0ffd52
Callum Arnold <24391445+arnold-c@users.noreply.github.com>
2023-10-30 10:10:39 -0400

    Export diag test colors

--- src/OutbreakDetection.jl
+++ src/OutbreakDetection.jl
@@ -66,9 +66,16 @@ export create_poisson_noise_arr, create_poisson_noise_arr!
 # @reexport using .NoiseFunctions
 
 include("plotting-functions.jl")
-export seircolors, seir_state_labels, create_sir_plot, draw_sir_plot,
+export seircolors,
+    seir_state_labels, create_sir_plot, draw_sir_plot,
+    DAILY_SENSITIVITY_COLOR, DAILY_SPECIFICITY_COLOR, DAILY_PPV_COLOR,
+    DAILY_NPV_COLOR, DETECTION_DELAY_COLOR, N_ALERTS_PER_OUTBREAK_COLOR,
+    N_FALSE_ALERTS_COLOR, N_ALERTS_COLOR, N_OUTBREAKS_COLOR,
+    N_MISSED_OUTBREAKS_COLOR, PERC_OUTBREAKS_DETECTED_COLOR,
+    PERC_OUTBREAKS_MISSED_COLOR, PERC_ALERTS_CORRECT_COLOR,
+    PERC_ALERTS_FALSE_COLOR,
     bifurcation_plot, bifurcation_heatmap,
-    sir_quantiles_array_base_plot, create_sir_quantiles_plot, outbreakcols,
+    sir_quantiles_array_base_plot, create_sir_quantiles_plot,
     incidence_prevalence_plot, visualize_ensemble_noise, incidence_testing_plot,
     testing_plot, ensemble_outbreak_distribution_plot, ensemble_OTChars_plot,
     singlescenario_test_positivity_plot, test_positivity_distribution_plot,
