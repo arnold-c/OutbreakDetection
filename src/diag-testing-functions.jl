3da8b23f8ec87b48b27a4b64db29a87179c4527a
Callum Arnold <24391445+arnold-c@users.noreply.github.com>
2023-11-14 21:23:09 -0500

    Rewrite calculate_movingavg function to split into multiple parts and remove type instability
    Previously relied on kwarg to determine whether the output was a float
    or integer vector. Splitting into multiple smaller functions allows use
    of multiple dispatch

--- src/diag-testing-functions.jl
+++ src/diag-testing-functions.jl
@@ -158,39 +158,67 @@ function calculate_positives!(
     return nothing
 end
 
-function calculate_movingavg(invec, avglag; Float = true)
-    if Float
-        outvec = zeros(Float64, size(invec, 1), 1)
-    else
-        outvec = zeros(Int64, size(invec, 1), 1)
-    end
+function calculate_movingavg(invec, avglag)
+    outvec = zeros(Float64, size(invec, 1))
 
-    calculate_movingavg!(outvec, invec, avglag; Float = Float)
+    calculate_movingavg!(outvec, invec, avglag)
 
     return outvec
 end
 
-function calculate_movingavg!(outvec, invec, avglag; Float = true)
+function calculate_movingavg(
+    invec::T1, avglag
+) where {T1<:AbstractArray{Integer}}
+    outvec = zeros(eltype(invec), size(invec, 1))
+
+    calculate_movingavg!(outvec, invec, avglag)
+
+    return outvec
+end
+
+function calculate_movingavg!(outvec, invec, avglag)
     if avglag == 0
         outvec .= invec
         return nothing
     end
 
-    if Float
-        avgfunc =
-            (invec, day, avglag) -> mean(@view(invec[(day - avglag + 1):day]))
-    else
-        avgfunc =
-            (invec, day, avglag) -> Int64(round(
-                mean(@view(invec[(day - avglag + 1):day]))
-            ))
-    end
-    for day in eachindex(invec)
-        if day >= avglag + 1
-            outvec[day] = avgfunc(invec, day, avglag)
-        end
+    @inbounds for day in eachindex(invec)
+        outvec[day] = calculate_float_daily_movingavg(invec, day, avglag)
     end
     return nothing
+end
+
+function calculate_movingavg!(
+    outvec::T1, invec, avglag
+) where {T1<:AbstractArray{<:Integer}}
+    if avglag == 0
+        outvec .= invec
+        return nothing
+    end
+
+    @inbounds for day in eachindex(invec)
+        outvec[day] = calculate_int_daily_movingavg(invec, day, avglag)
+    end
+    return nothing
+end
+
+function calculate_float_daily_movingavg(invec, day, avglag)
+    @inline moveavg_daystart = calculate_daily_movingavg_startday(day, avglag)
+    return mean(@view(invec[moveavg_daystart:day]))
+end
+
+function calculate_int_daily_movingavg(invec, day, avglag)
+    @inline moveavg_daystart = calculate_daily_movingavg_startday(day, avglag)
+    return Int64(round(mean(@view(invec[moveavg_daystart:day]))))
+end
+
+function calculate_daily_movingavg_startday(day, avglag)
+    if day < avglag
+        moveavg_daystart = 1
+    else
+        moveavg_daystart = day - avglag + 1
+    end
+    return moveavg_daystart
 end
 
 function detectoutbreak(incvec, avgvec, threshold)
