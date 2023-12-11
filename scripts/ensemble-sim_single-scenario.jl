5455cf31ba8ba97122066e386dc763bdcd589705
Callum Arnold <24391445+arnold-c@users.noreply.github.com>
2023-12-11 01:44:57 -0500

    Update single scenario to use a smaller % clinic tested

--- scripts/ensemble-sim_single-scenario.jl
+++ scripts/ensemble-sim_single-scenario.jl
@@ -32,7 +32,7 @@ ensemble_single_scenario_spec = ScenarioSpecification(
     ensemble_single_ensemble_spec,
     ensemble_single_outbreak_spec,
     ensemble_single_noise_spec,
-    OutbreakDetectionSpecification(5, 7, 0.6, 0.8),
+    OutbreakDetectionSpecification(5, 7, 0.6, 0.3),
     ensemble_single_individual_test_spec,
 )
 
@@ -58,7 +58,7 @@ ensemble_single_scenario_spec2 = ScenarioSpecification(
     ensemble_single_ensemble_spec,
     ensemble_single_outbreak_spec,
     ensemble_single_noise_spec,
-    OutbreakDetectionSpecification(10, 7, 0.6, 0.8),
+    OutbreakDetectionSpecification(10, 7, 0.6, 0.3),
     ensemble_single_individual_test_spec,
 )
 
