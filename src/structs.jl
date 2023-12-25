2a8ffd2a2e7f48348e17460de0bbd048376668f1
Callum Arnold <24391445+arnold-c@users.noreply.github.com>
2023-12-24 21:39:20 -0500

    Add R0 to simulation filepath

--- src/structs.jl
+++ src/structs.jl
@@ -182,6 +182,7 @@ function EnsembleSpecification(
         "N_$(state_parameters.init_states.N)",
         "r_$(state_parameters.init_state_props.r_prop)",
         "nsims_$(nsims)",
+        "R0_$(dynamics_parameters.R_0)",
         "vaccination_coverage_$(dynamics_parameters.vaccination_coverage)",
         "births_per_k_$(dynamics_parameters.annual_births_per_k)",
         "beta_force_$(dynamics_parameters.beta_force)",
