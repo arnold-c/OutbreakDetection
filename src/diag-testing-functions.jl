d4f882b076b9e70e0e7c985652738eba8d584b89
Callum Arnold <24391445+arnold-c@users.noreply.github.com>
2024-01-18 16:53:02 -0700

    Move all tests to respective functions in source code

--- src/diag-testing-functions.jl
+++ src/diag-testing-functions.jl
@@ -161,6 +161,64 @@ function calculate_positives!(
     return nothing
 end
 
+@testitem "Moving average" begin
+    using OutbreakDetection, Statistics
+
+    daily_testpositives = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+    movingavg_testpositives = calculate_movingavg(
+        daily_testpositives,
+        5,
+    )
+
+    @test isequal(
+        movingavg_testpositives,
+        [
+            mean([1]),
+            mean([1, 2]),
+            mean([1, 2, 3]),
+            mean([1, 2, 3, 4]),
+            mean([1, 2, 3, 4, 5]),
+            mean([2, 3, 4, 5, 6]),
+            mean([3, 4, 5, 6, 7]),
+            mean([4, 5, 6, 7, 8]),
+            mean([5, 6, 7, 8, 9]),
+            mean([6, 7, 8, 9, 10]),
+        ],
+    )
+
+    @test length(movingavg_testpositives) == length(daily_testpositives)
+
+    @test begin
+        daily_testpositives = zeros(Int64, 10, 2)
+        daily_testpositives[:, 1] .= [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+        calculate_movingavg!(
+            @view(daily_testpositives[:, 2]),
+            @view(daily_testpositives[:, 1]),
+            5,
+        )
+
+        isequal(
+            daily_testpositives[:, 2],
+            Int64.(
+                round.([
+                    mean([1]),
+                    mean([1, 2]),
+                    mean([1, 2, 3]),
+                    mean([1, 2, 3, 4]),
+                    mean([1, 2, 3, 4, 5]),
+                    mean([2, 3, 4, 5, 6]),
+                    mean([3, 4, 5, 6, 7]),
+                    mean([4, 5, 6, 7, 8]),
+                    mean([5, 6, 7, 8, 9]),
+                    mean([6, 7, 8, 9, 10]),
+                ])
+            ),
+        )
+    end
+end
+
 function calculate_movingavg(invec, avglag)
     outvec = zeros(Float64, size(invec, 1))
 
@@ -222,64 +280,6 @@ function calculate_daily_movingavg_startday(day, avglag)
         moveavg_daystart = day - avglag + 1
     end
     return moveavg_daystart
-end
-
-@testitem "Moving average" begin
-    using OutbreakDetection, Statistics
-
-    daily_testpositives = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-    movingavg_testpositives = calculate_movingavg(
-        daily_testpositives,
-        5,
-    )
-
-    @test isequal(
-        movingavg_testpositives,
-        [
-            mean([1]),
-            mean([1, 2]),
-            mean([1, 2, 3]),
-            mean([1, 2, 3, 4]),
-            mean([1, 2, 3, 4, 5]),
-            mean([2, 3, 4, 5, 6]),
-            mean([3, 4, 5, 6, 7]),
-            mean([4, 5, 6, 7, 8]),
-            mean([5, 6, 7, 8, 9]),
-            mean([6, 7, 8, 9, 10]),
-        ],
-    )
-
-    @test length(movingavg_testpositives) == length(daily_testpositives)
-
-    @test begin
-        daily_testpositives = zeros(Int64, 10, 2)
-        daily_testpositives[:, 1] .= [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-        calculate_movingavg!(
-            @view(daily_testpositives[:, 2]),
-            @view(daily_testpositives[:, 1]),
-            5,
-        )
-
-        isequal(
-            daily_testpositives[:, 2],
-            Int64.(
-                round.([
-                    mean([1]),
-                    mean([1, 2]),
-                    mean([1, 2, 3]),
-                    mean([1, 2, 3, 4]),
-                    mean([1, 2, 3, 4, 5]),
-                    mean([2, 3, 4, 5, 6]),
-                    mean([3, 4, 5, 6, 7]),
-                    mean([4, 5, 6, 7, 8]),
-                    mean([5, 6, 7, 8, 9]),
-                    mean([6, 7, 8, 9, 10]),
-                ])
-            ),
-        )
-    end
 end
 
 function detectoutbreak(incvec, avgvec, threshold)
@@ -543,6 +543,30 @@ function calculate_delay_vec(first_matchedbounds)
     return @views first_matchedbounds[:, 3] .- first_matchedbounds[:, 1]
 end
 
+@testitem "Matched bounds" begin
+    using OutbreakDetection
+
+    matched_bounds = [
+        10 60 5 15 600
+        10 60 17 40 600
+        10 60 50 80 600
+        100 180 90 105 700
+        100 180 110 160 700
+        380 410 390 420 900
+        500 540 495 550 1000
+    ]
+    filtered_bounds = [
+        10 60 5 15 600
+        100 180 90 105 700
+        380 410 390 420 900
+        500 540 495 550 1000
+    ]
+
+    @test isequal(
+        filter_first_matched_bounds(matched_bounds), filtered_bounds
+    )
+end
+
 function filter_first_matched_bounds(matchedbounds)
     indices = calculate_first_matched_bounds_index(matchedbounds)
     return matchedbounds[indices, :]
@@ -556,6 +580,40 @@ function calculate_first_matched_bounds_index(matchedbounds)
         ),
         unique(@view(matchedbounds[:, 1])),
     )
+end
+
+@testitem "Cases before and after alert" begin
+    using OutbreakDetection
+    @test begin
+        incarr = [
+            repeat([1], 9)...,
+            repeat([12], 51)...,
+            repeat([1], 39)...,
+            repeat([8], 81)...,
+            repeat([1], 199)...,
+            repeat([30], 31)...,
+            repeat([1], 89)...,
+            repeat([25], 41)...,
+        ]
+
+        matched_bounds = [
+            10 60 5 15 612
+            100 180 90 105 648
+            380 410 390 420 930
+            500 540 495 550 1025
+        ]
+
+        delay_vec = [-5, -10, 10, -5]
+        isequal(
+            calculate_cases_before_after_alert(
+                incarr, matched_bounds, delay_vec
+            ),
+            (
+                [612, 648, 630, 1025],
+                [1.0, 1.0, 630 / 930, 1.0],
+            ),
+        )
+    end
 end
 
 function calculate_cases_before_after_alert(
@@ -605,6 +663,66 @@ function calculate_cases_before_after_alert!(
         casesafterpercvec[alertnumber] = casesaftervec[alertnumber] / periodsum
     end
     return nothing
+end
+
+@testitem "Outbreak detection characteristics" begin
+    using OutbreakDetection
+    @test begin
+        outbreakbounds = [
+            2 4 500 100
+            10 60 600 10
+            100 180 700 2000
+            300 340 800 20
+            380 410 900 400
+            500 540 1000 5000
+        ]
+        detectionbounds = [
+            5 15
+            17 40
+            50 80
+            90 105
+            110 160
+            390 420
+            495 550
+            590 595
+        ]
+
+        isequal(
+            calculate_outbreak_detection_characteristics(
+                outbreakbounds, detectionbounds
+            ),
+            (
+                matched_bounds = [
+                    10 60 5 15 600
+                    10 60 17 40 600
+                    10 60 50 80 600
+                    100 180 90 105 700
+                    100 180 110 160 700
+                    380 410 390 420 900
+                    500 540 495 550 1000
+                ],
+                noutbreaks = 6,
+                nalerts = 8,
+                detected_outbreak_size = [600, 700, 900, 1000],
+                missed_outbreak_size = [500, 800],
+                n_true_outbreaks_detected = 4,
+                n_missed_outbreaks = 2,
+                n_correct_alerts = 7,
+                n_false_alerts = 1,
+                alertsperoutbreak = [0, 3, 2, 0, 1, 1],
+                periodsumvec = [500, 600, 700, 800, 900, 1000],
+                perc_true_outbreaks_detected = 4 / 6,
+                perc_true_outbreaks_missed = 2 / 6,
+                falsealert_trueoutbreak_prop = 1 / 6,
+                correctalert_trueoutbreak_prop = 7 / 6,
+                trueoutbreak_alerts_prop = 6 / 8,
+                outbreaksmissed_alerts_prop = 2 / 8,
+                perc_alerts_false = 1 / 8,
+                perc_alerts_correct = 7 / 8,
+                # detectiondelays = [-5, -10, 10, -5],
+            ),
+        )
+    end
 end
 
 # end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,4 @@
-using DrWatson, Test
+using DrWatson, TestItemRunner
 @quickactivate "OutbreakDetection"
 
 using OutbreakDetection
@@ -10,114 +10,7 @@ using OutbreakDetection
 println("Starting tests")
 ti = time()
 
-@testset "Detection tests" begin
-    @testset "Delay detection" begin
-        @test begin
-            matched_bounds = [
-                10 60 5 15 600
-                10 60 17 40 600
-                10 60 50 80 600
-                100 180 90 105 700
-                100 180 110 160 700
-                380 410 390 420 900
-                500 540 495 550 1000
-            ]
-            filtered_bounds = [
-                10 60 5 15 600
-                100 180 90 105 700
-                380 410 390 420 900
-                500 540 495 550 1000
-            ]
-            isequal(
-                filter_first_matched_bounds(matched_bounds), filtered_bounds
-            )
-        end
-        @test begin
-            outbreakbounds = [
-                2 4 500 100
-                10 60 600 10
-                100 180 700 2000
-                300 340 800 20
-                380 410 900 400
-                500 540 1000 5000
-            ]
-            detectionbounds = [
-                5 15
-                17 40
-                50 80
-                90 105
-                110 160
-                390 420
-                495 550
-                590 595
-            ]
-
-            isequal(
-                calculate_outbreak_detection_characteristics(
-                    outbreakbounds, detectionbounds
-                ),
-                (
-                    matched_bounds = [
-                        10 60 5 15 600
-                        10 60 17 40 600
-                        10 60 50 80 600
-                        100 180 90 105 700
-                        100 180 110 160 700
-                        380 410 390 420 900
-                        500 540 495 550 1000
-                    ],
-                    noutbreaks = 6,
-                    nalerts = 8,
-                    detected_outbreak_size = [600, 700, 900, 1000],
-                    missed_outbreak_size = [500, 800],
-                    n_true_outbreaks_detected = 4,
-                    n_missed_outbreaks = 2,
-                    n_correct_alerts = 7,
-                    n_false_alerts = 1,
-                    alertsperoutbreak = [0, 3, 2, 0, 1, 1],
-                    periodsumvec = [500, 600, 700, 800, 900, 1000],
-                    perc_true_outbreaks_detected = 4 / 6,
-                    perc_true_outbreaks_missed = 2 / 6,
-                    falsealert_trueoutbreak_prop = 1 / 6,
-                    correctalert_trueoutbreak_prop = 7 / 6,
-                    trueoutbreak_alerts_prop = 6 / 8,
-                    outbreaksmissed_alerts_prop = 2 / 8,
-                    perc_alerts_false = 1 / 8,
-                    perc_alerts_correct = 7 / 8,
-                    # detectiondelays = [-5, -10, 10, -5],
-                ),
-            )
-        end
-        @test begin
-            incarr = [
-                repeat([1], 9)...,
-                repeat([12], 51)...,
-                repeat([1], 39)...,
-                repeat([8], 81)...,
-                repeat([1], 199)...,
-                repeat([30], 31)...,
-                repeat([1], 89)...,
-                repeat([25], 41)...,
-            ]
-
-            matched_bounds = [
-                10 60 5 15 612
-                100 180 90 105 648
-                380 410 390 420 930
-                500 540 495 550 1025
-            ]
-
-            delay_vec = [-5, -10, 10, -5]
-            isequal(
-                calculate_cases_after_alert(incarr, matched_bounds, delay_vec),
-                (
-                    [612, 648, 630, 1025],
-                    [1.0, 1.0, 630 / 930, 1.0],
-                ),
-            )
-        end
-    end
-end
+@run_package_tests
 
 ti = time() - ti
 println("\nTest took total time of:")
