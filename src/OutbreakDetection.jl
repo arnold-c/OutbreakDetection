e13f79535bd7fd7d5006af4ebdd907c8b3b66cc8
Callum Arnold <24391445+arnold-c@users.noreply.github.com>
2024-06-04 15:21:43 -0400

    Move quantile plots to own file

--- src/OutbreakDetection.jl
+++ src/OutbreakDetection.jl
@@ -8,11 +8,13 @@ using Chain
 using NaNMath: NaNMath
 using FLoops
 
-
 include("plotting-helpers.jl")
 
 include("single-sim_plots.jl")
 export single_seir_plot, single_seir_statespace_plot, single_seir_beta_plot
+
+include("quantile-plots.jl")
+export create_seir_quantiles_plot
 
 include("ensemble-sim_single-scenario_plots.jl")
 
