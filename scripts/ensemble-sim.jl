4349d757ada631cb6efb6db170b0aa4a66d715f6
Callum Arnold <24391445+arnold-c@users.noreply.github.com>
2024-06-20 19:24:40 -0400

    Add simulations to run clinical case definitions

--- scripts/ensemble-sim.jl
+++ scripts/ensemble-sim.jl
@@ -142,7 +142,7 @@ test_spec_vec = [
     # IndividualTestSpecification(0.8, 0.8, 0),
     IndividualTestSpecification(0.85, 0.85, 0),
     IndividualTestSpecification(0.9, 0.9, 0),
-    # CLINICAL_TEST_SPECS...,
+    CLINICAL_TEST_SPECS...,
     IndividualTestSpecification(1.0, 1.0, 0),
     # IndividualTestSpecification(1.0, 1.0, 3),
     # IndividualTestSpecification(1.0, 1.0, 7),
