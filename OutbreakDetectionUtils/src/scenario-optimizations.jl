2a2952c47cf2a7c5ede9ab9fe1b5fe1436065cd6
Callum Arnold <24391445+arnold-c@users.noreply.github.com>
2025-03-13 17:04:06 -0400

    Add function to retrieve most recent optimization file

--- OutbreakDetectionUtils/src/scenario-optimizations.jl
+++ OutbreakDetectionUtils/src/scenario-optimizations.jl
@@ -36,7 +36,7 @@ function run_scenario_optimizations(
         mkpath(filedir)
     end
 
-    load_filepath = get_most_recent_hyperparam_filepath(
+    load_filepath = get_most_recent_optimization_filepath(
         optimization_filename_base,
         filedir,
     )
@@ -419,4 +419,70 @@ function run_missing_scenario_optimizations!(
     end
 
     return nothing
+end
+
+function get_most_recent_optimization_filepath(
+    filename_base,
+    filedir,
+)
+    @assert isdir(filedir)
+    optimization_files = readdir(filedir)
+
+    if length(optimization_files) == 0
+        return Try.Err("No optimization files found.")
+    end
+
+    filter_regex = Regex("(.*)$(filename_base)\$")
+
+    filtered_optimization_files = filter(
+        f -> contains(f, filter_regex),
+        optimization_files,
+    )
+
+    if length(filtered_optimization_files) == 0
+        return Try.Err("No optimization files found.")
+    end
+
+    filtered_optimization_datetimes = Vector{Union{Try.Ok,Try.Err}}(
+        undef, length(filtered_optimization_files)
+    )
+
+    for (i, f) in pairs(filtered_optimization_files)
+        matches = match(filter_regex, f)
+        if isnothing(matches)
+            filtered_optimization_datetimes[i] = Try.Err(
+                "No matches for filename $(f)"
+            )
+            continue
+        end
+
+        filtered_optimization_datetimes[i] = Try.Ok(
+            tryparse(
+                Dates.DateTime,
+                strip(
+                    matches[1],
+                    '_',
+                ),
+            ),
+        )
+    end
+
+    filtered_optimization_datetimes = filter(
+        Try.isok, filtered_optimization_datetimes
+    )
+
+    if length(filtered_optimization_datetimes) == 0
+        return Try.Err("No optimization files found.")
+    end
+
+    most_recent_optimization_datetime = sort(
+        Try.unwrap.(filtered_optimization_datetimes)
+    )[end]
+
+    most_recent_filepath = joinpath(
+        filedir,
+        string(most_recent_optimization_datetime) *
+        "_$(filename_base)",
+    )
+    return Try.Ok(most_recent_filepath)
 end
