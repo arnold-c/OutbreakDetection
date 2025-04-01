e6fe05b0d9fbb9eaa2a4851a623369c55035c958
Callum Arnold <24391445+arnold-c@users.noreply.github.com>
2025-04-01 16:23:51 -0400

    Add method to display multiple characteristics

--- manuscript/scripts/optimal-thresholds_checks.jl
+++ manuscript/scripts/optimal-thresholds_checks.jl
@@ -1,6 +1,27 @@
 #%%
 function compare_optimal_solution_extrema(
     optimal_solutions_vec,
+    characteristic_vec::AbstractVector{T},
+    test_spec_vec = [
+        IndividualTestSpecification(1.0, 1.0, 0),
+        IndividualTestSpecification(0.9, 0.9, 0),
+        IndividualTestSpecification(0.85, 0.85, 0),
+    ],
+) where {T<:Symbol}
+    for c in characteristic_vec
+        Base.display(
+            compare_optimal_solution_extrema(
+                optimal_solutions_vec,
+                c,
+                test_spec_vec,
+            ),
+        )
+        println()
+    end
+end
+
+function compare_optimal_solution_extrema(
+    optimal_solutions_vec,
     characteristic::Symbol,
     test_spec_vec = [
         IndividualTestSpecification(1.0, 1.0, 0),
@@ -10,13 +31,14 @@ function compare_optimal_solution_extrema(
 )
     @assert length(optimal_solutions_vec) == length(test_spec_vec)
 
+    println("Characteristic: $characteristic =======================")
     return mapreduce(
         ((optimal_solutions, test_spec),) -> extract_optimal_solution_extrema(
             optimal_solutions,
             characteristic,
             test_spec,
         ),
-        vcat,
+        merge,
         zip(optimal_solutions_vec, test_spec_vec),
     )
 end
@@ -40,7 +62,7 @@ function extract_optimal_solution_extrema(
             );
             digits = digits,
         )
-    return eval(:(Dict($test_spec => $test_extrema)))
+    return Dict(test_spec => test_extrema)
 end
 
 function extract_test_optimal_solutions(optimal_solutions, test_spec)
@@ -56,12 +78,6 @@ compare_optimal_solution_extrema(
     vcat(
         [perfect_test_optimal_solutions],
         repeat([dynamical_noise_rdt_optimal_solutions], 2),
-    ), :detectiondelays
-)
-
-compare_optimal_solution_extrema(
-    vcat(
-        [perfect_test_optimal_solutions],
-        repeat([dynamical_noise_rdt_optimal_solutions], 2),
-    ), :alert_duration_vec
+    ),
+    [:detectiondelays, :alert_duration_vec],
 )
