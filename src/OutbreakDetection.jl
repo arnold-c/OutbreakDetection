bf9e9abdb876571a668244748f8dc1ab02c1e12a
Callum Arnold <24391445+arnold-c@users.noreply.github.com>
2025-11-13 15:29:38 -0500

    src: remove unused noise visualization plotting module
    Delete noise_plots.jl containing visualize_ensemble_noise. This
    function was only called by plot_all_single_scenarios which was
    removed in a previous revision.
    
    This is a structural change with no behavioral impact.

--- src/OutbreakDetection.jl
+++ src/OutbreakDetection.jl
@@ -18,9 +18,6 @@ export ACCURACY_COLOR, DAILY_SENSITIVITY_COLOR, DAILY_SPECIFICITY_COLOR,
     PERC_OUTBREAKS_MISSED_COLOR, PERC_ALERTS_CORRECT_COLOR,
     PERC_ALERTS_FALSE_COLOR
 
-include("noise_plots.jl")
-export visualize_ensemble_noise
-
 include("outbreak-threshold-chars_plots.jl")
 export ensemble_OTChars_plot, save_compare_ensemble_OTchars_plot,
     compare_ensemble_OTchars_plots, ensemble_outbreak_detect_diff_plot,
