d4dbb685b0778a864366257c8dc7717a18a46322
Callum Arnold <24391445+arnold-c@users.noreply.github.com>
2026-01-19 17:13:44 -0500

    delete: OutbreakThresholdChars struct as not used
    remove as previous version saved too much data that wasn't
    ultimately used in the analysis for the manuscript

--- OutbreakDetectionCore/src/types/outbreak-specifications.jl
+++ OutbreakDetectionCore/src/types/outbreak-specifications.jl
@@ -1,5 +1,4 @@
-export OutbreakSpecification,
-    OutbreakThresholdChars
+export OutbreakSpecification
 
 """
     OutbreakSpecification
@@ -42,63 +41,4 @@ function OutbreakSpecification(
         minimum_outbreak_size,
         dirpath,
     )
-end
-
-"""
-    OutbreakThresholdChars
-
-Characteristics of outbreak detection performance for a given threshold.
-
-Contains comprehensive metrics about detection accuracy, timing, and resource usage.
-"""
-struct OutbreakThresholdChars{
-        T1 <: AbstractFloat,
-        T2 <: Integer,
-        T3 <: Vector{<:AbstractFloat},
-        T4 <: Vector{<:Integer},
-        T5 <: AbstractMatrix{<:Integer},
-    }
-    daily_sensitivity::T1
-    daily_specificity::T1
-    daily_ppv::T1
-    daily_npv::T1
-    accuracy::T1
-    f1_score::T1
-    matchedoutbreakbounds::T5
-    noutbreaks::T2
-    nalerts::T2
-    outbreak_duration_vec::T4
-    alert_duration_vec::T4
-    detected_outbreak_size::T4
-    missed_outbreak_size::T4
-    n_true_outbreaks_detected::T2
-    n_missed_outbreaks::T2
-    n_correct_alerts::T2
-    n_false_alerts::T2
-    n_alerts_per_outbreak::T4
-    period_sum_per_outbreak::T4
-    perc_true_outbreaks_detected::T1
-    perc_true_outbreaks_missed::T1
-    falsealert_trueoutbreak_prop::T1
-    correctalert_trueoutbreak_prop::T1
-    trueoutbreak_alerts_prop::T1
-    outbreaksmissed_alerts_prop::T1
-    perc_alerts_false::T1
-    perc_alerts_correct::T1
-    detectiondelays::T4
-    cases_before_alerts::T4
-    cases_perc_before_alerts::T3
-    cases_after_alerts::T4
-    cases_perc_after_alerts::T3
-    unavoidable_cases::T2
-    avoidable_cases::T2
-    n_outbreak_cases::T2
-    n_tests::T2
-    n_outbreak_tests::T2
-    mean_noise_incidence_ratio::T1
-    mean_poisson_noise::T1
-    poisson_noise_prop::T1
-    proportion_timeseries_in_outbreak::T1
-    proportion_timeseries_in_alert::T1
-    alert_outbreak_timeseries_prop_diff::T1
 end
